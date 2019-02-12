a841541fd9a8e19d6e8530e7c62273364300023f
xmiao <xmiao@PC0L6SQ2.corp.hds.com>
2019-02-12 14:13:59 +0800

    对比这个貌似不太一样

--- github20190212test/src/github20190212test/test1.java
+++ github20190212test/src/github20190212test/test1.java
@@ -8,6 +8,8 @@ public class test1 {
 		System.out.println("haha");
 		//做一些更改，看看类文件会变成什么。
 		System.out.println("类文件会不会变化呢！");
+		//现在要进行对比
+		System.out.println("进行compare with》git index对比");
 	}
 
 }
